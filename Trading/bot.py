b707b174fb904f37abc5bb75ac26e257ecff043a
Karl <karltimmins@gmail.com>
2020-10-11 20:34:05 +0000

    Initial commit to include flask app + .gitignore

--- Trading/bot.py
+++ Trading/bot.py
@@ -1,9 +1,134 @@
-from alpha_vantage.timeseries import Timeseries
+import requests
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from datetime import timedelta
+from datetime import datetime
+from key import api_key
+import time
 
-key = '0656f4c261msh66918c48b22e843p1a7d6fjsn6b93574f99f2'
+def request_stock_price_hist(symbol, token, sample = False):
+    """
+    This function helps the user to retrieve historical stock prices for the
+    specified symbol from Alpha Vantage.
+    Parameters
+    ----------
+    symbol : String
+        Stock symbol, e.g. Apple is AAPL.
+    token : String
+        Register an account on alphavantage.co and get your API.
+    sample : Boolean, optional
+        Set to True to take a sample of the data only.
+    Returns
+    -------
+    df : Pandas DataFrame
+        A Pandas DataFrame containing stock price information.
+    """
+    if sample == False:
+        q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={APPL}&outputsize=full&apikey={api_key}'
+    else:
+        q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={APPL}&apikey={api_key}'
+    
+    print("Retrieving stock price data from Alpha Vantage (This may take a while)...")
+    r = requests.get(q_string.format(symbol, token))
+    print("Data has been successfully downloaded...")
+    date = []
+    colnames = list(range(0, 7))
+    df = pd.DataFrame(columns = colnames)
+    print("Sorting the retrieved data into a dataframe...")
+    for i in tqdm(r.json()['Time Series (Daily)'].keys()):
+        date.append(i)
+        row = pd.DataFrame.from_dict(r.json()['Time Series (Daily)'][i], orient='index').reset_index().T[1:]
+        df = pd.concat([df, row], ignore_index=True)
+    df.columns = ["open", "high", "low", "close", "adjusted close", "volume", "dividend amount", "split cf"]
+    df['date'] = date
+    return df
 
-ts = Timeseries(key)
+def request_quote(symbol, token):
+    """
+    This function helps the user to retrieve current stock quote for the
+    specified symbol from Alpha Vantage.
+    Parameters
+    ----------
+    symbol : String
+        Stock symbol, e.g. Apple is AAPL.
+    token : String
+        Register an account on alphavantage.co and get your API.
+    Returns
+    -------
+    df : Pandas DataFrame
+        A Pandas DataFrame containing stock price information.
+    """
+    q_string = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey={}'
+    r = requests.get(q_string.format(symbol, token))
+    colnames = [x.split('. ')[1] for x in list(r.json()['Global Quote'].keys())]
+    df = pd.DataFrame.from_dict(r.json()['Global Quote'], orient='index').reset_index().T[1:]
+    df.columns = colnames
+    return df
 
-aapl, meta = ts.get.daily(symbol='AAPL')
+def request_symbols(token):
+    """
+    This function helps the user to retrieve all currently active listed 
+    stocks and their symbols from Alpha Vantage.
+    Parameters
+    ----------
+    token : String
+        Register an account on alphavantage.co and get your API.
+    Returns
+    -------
+    df : Pandas DataFrame
+        A Pandas DataFrame containing stock price information.
+    """
+    q_string = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}'   
+    print("Retrieving stock symbols from Alpha Vantage...")
+    r = requests.get(q_string.format(token)).content
+    print("Data has been successfully downloaded...")
+    r = r.decode("utf-8")
+    colnames = list(range(0, 6))
+    df = pd.DataFrame(columns = colnames)
+    print("Sorting the retrieved data into a dataframe...")
+    for i in tqdm(range(1, len(r.split('\r\n'))-1)):
+        row = pd.DataFrame(r.split('\r\n')[i].split(',')).T
+        df = pd.concat([df, row], ignore_index=True)
+    df.columns = r.split('\r\n')[0].split(',')
+    return df
 
-print(aapl['2020-08-21'])
+#symb = list(sym_df.loc[sym_df['exchange'] == 'NASDAQ']['symbol'])
+#save_stock_price_hist(symb[116:200], token, 'Data')
+
+def save_stock_price_hist(symbol_list, token, pwd=''):
+    """
+    This function helps the user to download all historical stock prices
+    for each symbol on the symbol_list parameter from Alpha Vantage. There 
+    will be a CSV file for each symbol and the user can specify the output 
+    path for the CSV files.
+    
+    Please note that Alpha Vantage does not accept more than 5 calls per
+    minute. Therefore if your symbol_list object is long, the function will
+    automatically set waiting time to ensure there is no break once the
+    function is set to run.
+    Parameters
+    ----------
+    symbol_list : List
+        A list of symbols (string type) to query.  
+    token : String
+        Register an account on alphavantage.co and get your API.
+    pwd : String, optional
+        The path to the directory where the CSV files will be saved.
+    Returns
+    -------
+    None
+    """
+    n = datetime.now()
+    curr = 0
+    for i in symbol_list:
+        ind = symb.index(i)
+        l = datetime.now()
+        if (l - n < timedelta(minutes=1)) and (ind-curr == 5):
+            print("We made 5 API calls in the last minute, taking a break...")
+            time.sleep((timedelta(minutes=2) - (l - n)).seconds)
+            curr = i
+        else:
+            df = request_stock_price_hist(i, token)
+            df.to_csv('{}/{}_{}.csv'.format(pwd, i, datetime.today().strftime('%Y%m%d')))
+    return None
--- Trading/index.py
+++ Trading/index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from key import api_key
+import matplotlib.pyplot as plt
 from alpha_vantage.techindicators import TechIndicators
 from alpha_vantage.timeseries import TimeSeries
 
@@ -11,20 +12,9 @@ def rsi_dataframe(stock=stock):
     api_key = 'api_key'
     period = 60
     ts = TimeSeries(key=api_key, output_format='pandas')
-    data_ts = ts.get_intraday(stock.upper(), interval='1min', outputsize='full')
-
-#indicator
-
-    ti = TechIndicators(key='api_key', output_format='pandas')
-    data_ti, meta_data_ti = ti.get_rsi(symbol=stock.upper(), interval='1min', time_period=period, series_type='close')
+    data_ts = ts.get_daily(stock.upper(), outputsize='full')
 
     df = data_ts[0],[period]
-
-    #df.index = pd.index(map(lambda x: str(x)[:-3], df.index))
-
-    #df2 = data_ti
-
-    #total_df = pd.concat([df,df2],axis=1,sort=True)
 
     return print(df)
 
