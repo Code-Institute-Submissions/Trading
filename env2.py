ce43907529edc49a8059109c02aa437043075801
Karl <karltimmins@gmail.com>
2020-10-14 22:11:46 +0000

    removal of env2.py

--- env2.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "!,(5Ssg!#r&|Z>.o,:p5>8]lv@Ob]j")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://karldb1:August1987@cluster0.hqmny.mongodb.net/Trading-data-set?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "Trading-data-set")
